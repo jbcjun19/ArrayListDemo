48ebe533a07158a7a650fd56a514b57eb2cb5d1c
jnfrle <lejennaires@gmail.com>
2019-06-19 10:02:41 -0400

    better readability

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -33,20 +33,22 @@ public class Main {
         myArr.add("tom");
 
         System.out.println("The length of my list is " + myArr.size());
-        System.out.println(myArr);
+        System.out.println("Current list: " + myArr + "\n");
 
         System.out.println("Is 'paul' in my list?: " + myArr.contains("paul"));
         System.out.println("Is 'jake' in my list?: " + myArr.contains("jake"));
 
         myArr.remove(1);
         myArr.remove("tom");
+        System.out.println("Current list: " + myArr + "\n");
 
         System.out.println("Who is at the beginning of the list?: " + myArr.get(0));
-        System.out.println("Where in my list is 'lauren'?: " + myArr.indexOf("lauren"));
+        System.out.println("What's the index position of 'lauren'?: " + myArr.indexOf("lauren"));
 
         myArr.clear();
-        System.out.println(myArr);
+        System.out.println("Current list: " + myArr + "\n");
 
+        System.out.println("Calling a method...");
         howToAddAllToArrayList();
 
     }
